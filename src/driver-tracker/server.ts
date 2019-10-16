90ed286c2f3967dfa270cadc5308be9ff0b2399d
Muhamad Hilmi Hibatullah <hilmihibatullah@gmail.com>
2019-10-16 10:41:56 +0700

    menambahkan encoding track

---
+++ src/driver-tracker/server.ts
@@ -0,0 +1,23 @@
+import * as express from 'express'
+import * as cors from 'cors'
+import {createServer} from 'http'
+import {json as jsonBodyParser} from 'body-parser'
+import { Server } from 'net';
+import {track} from './track'
+
+const PORT = process.env['RH_PORT'];
+
+const app = express();
+app.set('port', PORT)
+app.use(cors());
+
+// routing
+app.post('/track', jsonBodyParser(), track)
+
+const server = createServer(app);
+
+export function  startServer(): Server {
+    return server.listen(PORT, () => {
+        console.log('server listen on port ', PORT);
+    });
+}
---
+++ src/driver-tracker/track.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+
+export function track(req: Request, res: Response) {
+    //parsing input
+    const {north, west, east, south} = req.body;
+    if(!north || !west  || !east || !south){
+        res.status(400).json({
+            ok: false,
+            error: 'parameter tidak lengkap'
+        });
+        return;
+    }
+
+    //encode output
+    res.json({
+        ok: true
+    });
+}
