c75c5a51fa47d3b1e369b200900906fda7f34d20
Muhamad Hilmi Hibatullah <hilmihibatullah@gmail.com>
2019-10-16 11:12:26 +0700

    simpan perpindahan driver

---
+++ src/driver-tracker/orm.ts
@@ -0,0 +1,26 @@
+import { Sequelize, DataTypes, Model } from 'sequelize'
+
+const db = new Sequelize({
+    database: 'ridehailing',
+    username: 'postgres',
+    password: '12345678',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    logging: false
+});
+
+export class TrackEvent extends Model {}
+TrackEvent.init({
+        rider_id: DataTypes.INTEGER,
+        north: DataTypes.FLOAT,
+        west: DataTypes.FLOAT,
+        east: DataTypes.FLOAT,
+        south: DataTypes.FLOAT
+    },
+    { modelName: "track_event", sequelize: db }
+);
+
+export function syncDB(): Promise<Sequelize> {
+    return db.sync();
+}
