401ed99e04c44214542985c6f95513321506f117
Muhammad Hasan <hasan@sirus.dev>
2019-10-16 10:41:42 +0700

    menambah endpoint track

--- src/driver-tracker/server.ts
+++ src/driver-tracker/server.ts
@@ -2,12 +2,17 @@ import * as express from 'express';
 import * as cors from 'cors';
 import { createServer } from 'http';
 import { Server } from 'net';
+import { track } from './track';
+import { json as jsonBodyParser } from 'body-parser';
 
-const PORT = process.env['RH_PORT'];
+const PORT = process.env['RH_PORT'] || 3000;
 
 const app = express();
 app.set('port', PORT);
 app.use(cors());
+
+// routing
+app.post('/track', jsonBodyParser(), track);
 
 const server = createServer(app);
 
