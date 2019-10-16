f9422c3de4557b2bb574036690f51ea2d655c6c3
unknown <andria.rezaandria.reza74@gmail.com>
2019-10-16 11:29:17 +0700

    update posisi setelah berpindah

--- src/driver-tracker/track.ts
+++ src/driver-tracker/track.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { TrackEvent } from './orm';
+import { TrackEvent, DriverPosition } from './orm';
 
 export async function track(req: Request, res: Response) {
     // PARSING INPUT
@@ -33,6 +33,34 @@ export async function track(req: Request, res: Response) {
         return;
     }
     
+    // UPDATE DRIVER POSITION
+    const [position, created] = await
+    DriverPosition.findOrCreate({
+        defaults: {
+            latitude: 0,
+            longitude: 0
+        }, where: {
+            rider_id
+        }
+    });
+    let latitude = position.get('latitude');
+    latitude = latitude + north - south;
+    let longitude = position.get('longitude');
+    longitude = longitude + east - west;
+    
+    try {
+        await position.update({
+            latitude,
+            longitude
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            ok: false,
+            message: "gagal menyimpan posisi driver"
+        });
+        return;
+    }
 
     // ENCODE INPUT
     res.json({
