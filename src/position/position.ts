4cd49bef2a32ccb6e164068b60d8a8fe9fd6b4d3
unknown <andria.rezaandria.reza74@gmail.com>
2019-10-17 15:48:00 +0700

    menambahkan fitur point

--- src/position/position.ts
+++ src/position/position.ts
@@ -4,27 +4,46 @@ import { NatsError } from 'nats';
 
 interface Movement {
     rider_id: number;
-    north: number;
-    west: number;
-    east: number;
-    south: number;
+    north: string;
+    west: string;
+    east: string;
+    south: string;
+}
+
+export async function showPosition(req, res) {
+    const rider_id = req.params.rider_id;
+    console.log('showPosition param: '+rider_id);
+
+    try{
+        const show = await DriverPosition.findAll({
+            where: {
+                rider_id: rider_id
+            }
+        });
+        console.log('isi show: '+show[0]);
+        res.send(`{ ${show[0]['latitude']}, ${show[0]['longitude']} }`)
+    }catch(err){
+        console.log('error di showPosition: '+err);
+    }
 }
 
 export async function positionUpdater(movement: Movement) {
-    const {
-        north, west, east, south, rider_id
-    } = movement;
+    const rider_id = movement.rider_id;
+    const north = parseFloat(movement.north);
+    const west = parseFloat(movement.west);
+    const east = parseFloat(movement.east);
+    const south = parseFloat(movement.south);
 
     // UPDATE DRIVER POSITION
-    const [position, created] = await
-    DriverPosition.findOrCreate({
+    const [position, created] = await DriverPosition.findOrCreate({
         defaults: {
-            latitude: 0,
-            longitude: 0
-        }, where: {
-            rider_id
+          latitude: 0,
+          longitude: 0
+        },
+        where: {
+          rider_id
         }
-    });
+      });
 
     // UPDATE LONGITUDE LATITUDE
     let latitude = parseFloat(position.get("latitude") as string);
@@ -34,11 +53,11 @@ export async function positionUpdater(movement: Movement) {
     
     try {
         await position.update({
-            latitude,
-            longitude
-        });
-    } catch(err) {
-        console.log(err);
+        latitude,
+        longitude
+    });
+    } catch (err) {
+    console.error(err);
     }
 }
 
